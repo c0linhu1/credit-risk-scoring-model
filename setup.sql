6035a5ee6165b88a81cadde9167c385bd4e12807
c0linhu1 <141664358+c0linhu1@users.noreply.github.com>
2025-12-14 19:22:24 -0500

    setup db done - initial single flat 'loans' table

--- setup.sql
+++ setup.sql
@@ -1,25 +1,22 @@
 /*
 setup.sql
-creates staging table, loads Kaggle CSV data, and builds normalized 3-table schema (customers, loans, defaults)
+creates staging table, loads Kaggle CSV data, and builds table 'loans'
 */
 
 
 -- dropping table if already exists so we can run over and over again with a clean base
     -- CASCADE deletes objects that depend on the table 
-DROP TABLE IF EXISTS defaults CASCADE;
 DROP TABLE IF EXISTS loans CASCADE;
-DROP TABLE IF EXISTS customers CASCADE;
-DROP TABLE IF EXISTS credit_risk_staging CASCADE;
+
 
 -- creating table to load csv - same columns
-CREATE TABLE credit_risk_staging (
+CREATE TABLE loans (
     -- person_age: how old the person is
     -- each line is one column in the table
     person_age INT,
     
     -- person_income: How much money they make per year
-    -- max 12 digits allowed w 2 digits in the decimals
-    person_income DECIMAL(12,2),
+    person_income INT,
     
     -- person_home_ownership: do they rent, own, have mortgage, etc.
     -- max 20 characters
@@ -31,18 +28,15 @@ CREATE TABLE credit_risk_staging (
     person_emp_length DECIMAL(5,2),
     
      -- loan_intent: reason for loan
-    -- max 50 characters
-    -- Ex: "EDUCATION", "MEDICAL", "PERSONAL", "HOMEIMPROVEMENT"
-    loan_intent VARCHAR(50),
+    -- ['DEBTCONSOLIDATION', 'EDUCATION', 'HOMEIMPROVEMENT', 'MEDICAL', 'PERSONAL', 'VENTURE']
+    loan_intent VARCHAR(30),
 
     -- loan_grade: letter grade rating loan quality 
-    -- max 5 chars to be safe 
-    -- EX: "A", "B", "C", "D", "E", "F", "G" - 
-    loan_grade VARCHAR(5),
+    -- ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    loan_grade VARCHAR(1),
 
     -- loan_amnt: How much money they want to borrow
-    -- max 12 digits w 2 digits in decimals
-    loan_amnt DECIMAL(12,2),
+    loan_amnt INT,
         
     -- loan_int_rate: Interest rate on the loan (percentage)
     -- max 5 digits w 2 decimal places
@@ -57,10 +51,9 @@ CREATE TABLE credit_risk_staging (
     
     -- loan_percent_income: loan amount as a percentage of yearly income
         -- also known as DTI (debt-to-income ratio)
-    -- This is also called DTI (Debt-to-Income ratio)
-    -- max 5 digits w 4 decimal places
+    -- max 4 digits w 2 decimal places
     -- ex: 0.25 -> 25% of income
-    loan_percent_income DECIMAL(5,4),
+    loan_percent_income DECIMAL(3,2),
     
     -- cb_person_default_on_file: if person has defaulted before
             -- "cb" -> "credit bureau"
@@ -75,7 +68,7 @@ CREATE TABLE credit_risk_staging (
     
 );
 
-\copy credit_risk_staging FROM '/Users/colin/Downloads/SQLproject/credit_risk_dataset.csv' DELIMITER ',' CSV HEADER
+\copy loans FROM '/Users/colin/Downloads/SQLproject/credit_risk_dataset.csv' DELIMITER ',' CSV HEADER
 
 
 -- Verify data loaded correctly
@@ -94,8 +87,8 @@ SELECT
     SUM(loan_status) as total_defaults,
     -- calculating default rate percentage
     ROUND(100.0 * SUM(loan_status) / COUNT(*), 2) as default_rate_pct
--- selecting data FROM the credit_risk_staging table after copying data from csv into that table    
-FROM credit_risk_staging;
+-- selecting data FROM the loans table after copying data from csv into that table    
+FROM loans;
 
 -- trying to see nulls in important columns
 SELECT
@@ -103,64 +96,21 @@ SELECT
     -- COUNT(*) = total rows, COUNT(person_age) = non-null rows
     COUNT(*) - COUNT(person_age) as null_count,
     ROUND(100.0 * (COUNT(*) - COUNT(person_age)) / COUNT(*), 2) as null_pct
-FROM credit_risk_staging
+FROM loans
 -- we use UNION ALL because we want all columns 
 UNION ALL
 SELECT 'person_income', 
        COUNT(*) - COUNT(person_income), 
        ROUND(100.0 * (COUNT(*) - COUNT(person_income)) / COUNT(*), 2)
-FROM credit_risk_staging
+FROM loans
 UNION ALL
 SELECT 'person_emp_length', 
        COUNT(*) - COUNT(person_emp_length),
        ROUND(100.0 * (COUNT(*) - COUNT(person_emp_length)) / COUNT(*), 2)
-FROM credit_risk_staging
+FROM loans
 UNION ALL
 SELECT 'loan_int_rate', 
        COUNT(*) - COUNT(loan_int_rate),
        ROUND(100.0 * (COUNT(*) - COUNT(loan_int_rate)) / COUNT(*), 2)
-FROM credit_risk_staging;
+FROM loans;
 
-
--- Create customers table
-CREATE TABLE customers AS
-SELECT 
-    -- window function - assigning numbers for customer_id to randomly ordered customers
-    ROW_NUMBER() OVER (ORDER BY RANDOM()) as customer_id,
-    person_age,
-    person_income,
-    person_home_ownership,
-    -- COALESCE: if person_emp_length is null, replace with 0 otherwise use the actual value
-    COALESCE(person_emp_length, 0) as person_emp_length,
-    cb_person_default_on_file as historical_default,
-    cb_person_cred_hist_length as credit_history_length,
-    -- I want to add region data to this dataset so we can experiment
-    -- and we can analyze different aspects based on region
-    -- This basically will generate a random int value from 0-4 
-    CASE (RANDOM() * 5)::INT
-        WHEN 0 THEN 'Northeast'
-        WHEN 1 THEN 'Southeast'
-        WHEN 2 THEN 'Midwest'
-        WHEN 3 THEN 'Southwest'
-        ELSE 'West'
-    END as region
-FROM credit_risk_staging;
-
--- creating a primary key makes sure that every customer has a unique id - preventing duplicates
--- also apparently queries are faster with primary keys and we can join tables easier
-ALTER TABLE customers ADD PRIMARY KEY (customer_id);
-
--- making sure we dont have any negative values in our data - would be impossible making data inaccurate
-ALTER TABLE customers ADD CONSTRAINT chk_income CHECK (person_income >= 0);
-ALTER TABLE customers ADD CONSTRAINT chk_emp_length CHECK (person_emp_length >= 0);
-
--- ONLY CREATE INDEXES ON COLUMNS WE OFTEN FILTER, JOIN ON, ORDER OR GROUP BY, AND HAS MANY UNIQUE VALUES 
-CREATE INDEX idx_customers_income ON customers(person_income);
-CREATE INDEX idx_customers_age ON customers(person_age);
-CREATE INDEX idx_customers_region ON customers(region);
-
--- this is info to remind myself 
-COMMENT ON TABLE customers IS 'Customer demographic and credit history information';
-COMMENT ON COLUMN customers.customer_id IS 'Unique customer identifier (Primary Key)';
-COMMENT ON COLUMN customers.historical_default IS 'Y = previous default history, N = no history';
-COMMENT ON COLUMN customers.credit_history_length IS 'Length of credit history in years';
